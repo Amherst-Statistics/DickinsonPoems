b538342de97c52a3d37e5cfdd53dd33618254d3b
Nicholas Horton <nhorton@amherst.edu>
2019-10-16 10:59:20 -0400

    Fixed stupid error.

--- R/get_poem.R
+++ R/get_poem.R
@@ -1,7 +1,7 @@
 get_poem <- function(poem) {
   filename <- paste(system.file("extdata", package = "DickinsonPoems"), 
                   poem, sep = "/") 
-  stopifnot(file.exists("filename"))
+  stopifnot(file.exists(filename))
   poemLines <- readLines(filename)
   return(poemLines)
 }
